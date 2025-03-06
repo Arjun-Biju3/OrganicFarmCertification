863e798fe84f1d5c39733187102174054bff6f65
Arjun Biju <arjunbiju332@gmail.com>
2025-03-06 13:23:51 +0530

    remove mongoose unique validatoe

--- backend/models/place.js
+++ backend/models/place.js
@@ -1,19 +1,17 @@
 const mongoose = require('mongoose');
 
-
 const Schema = mongoose.Schema;
 
-
-const placeScheme = new Schema({
-    title:{type:String,required:true},
-    description:{type:String,required:true},
-    image:{type:String,required:true},
-    address:{type:String,required:true},
-    location:{
-        lat: {type:Number, required:true},
-        lon: {type:Number, required:true},
+const placeSchema = new Schema({
+    title: { type: String, required: true },
+    description: { type: String, required: true },
+    image: { type: String, required: true },
+    address: { type: String, required: true },
+    location: {
+        lat: { type: Number, required: true },
+        lon: { type: Number, required: true }, // Keeping 'lon' as per original code
     },
-    creator:{type:mongoose.Types.ObjectId,required:true,ref:'user'},
+    creator: { type: mongoose.Types.ObjectId, required: true, ref: 'User' }, // Fixed reference
 });
 
-module.exports = mongoose.model('Place',placeScheme);
+module.exports = mongoose.model('Place', placeSchema);
